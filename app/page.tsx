8ba55896b88bbd544479191bd6a14737d7723889
Guido Boetsch <gboetsch93@gmail.com>
2026-08-02 16:12:56 -0300

    Update wedding itinerary schedule

--- app/page.tsx
+++ app/page.tsx
@@ -280,13 +280,14 @@ export default function Home() {
       <section className="timeline" id="itinerario" aria-labelledby="timeline-title">
         <div className="timeline-heading">
           <p className="eyebrow light">El itinerario</p>
-          <h2 id="timeline-title">Así se vivirá<br />este mágico día.</h2>
+          <h2 id="timeline-title">Así se vivirá<br />esta noche.</h2>
         </div>
         <ol>
-          <li><time>17:30</time><span>Recepción</span><em>Empezamos a brindar</em></li>
+          <li><time>17:30</time><span>Recepción</span><em>Los esperamos previo a la ceremonia</em></li>
           <li><time>18:00</time><span>Ceremonia</span><em>El sí más esperado</em></li>
           <li><time>19:00</time><span>Cóctel</span><em>Comida, fotos y abrazos</em></li>
-          <li><time>21:00</time><span>Cena + fiesta</span><em>Hasta que salga el sol</em></li>
+          <li><time>20:30</time><span>ARRANCA LA FIESTA</span><em>Hay morfi, baile y mucho más!</em></li>
+          <li><time>3:30</time><span>FIN</span><em>... o hasta que nos saquen</em></li>
         </ol>
         <img className="timeline-rabbits" src="/rabbits-nyf-v2.png" alt="Dos conejos abrazados" />
       </section>
